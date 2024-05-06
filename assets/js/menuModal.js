150c5b116457382c133517610b7ecb9c9c4d8397
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-05-06 03:55:46 +0000

    update menu1

---
+++ assets/js/menuModal.js
@@ -0,0 +1,17 @@
+// récupération des éléments js 
+let infoBtn = document.getElementById("menu-modal");
+let closeBtn = document.getElementById("menu-modal-close");
+
+// ouvrir la modale
+infoBtn.addEventListener("click", (e) => {
+    // ouverture de la fenêtre modale
+    document.querySelector(".landing-modal-window").style.display = "block";
+    document.querySelector(".custom-filter-modal").style.display = "block";
+})
+
+// // fermer la modale
+closeBtn.addEventListener("click", (e)=> {
+    // fermeture de la modale
+    document.querySelector(".landing-modal-window").style.display = "none";
+    document.querySelector(".custom-filter-modal").style.display = "none";
+})
