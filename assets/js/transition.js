5d0af3ec7e3848182b5dffb716f7ef4f8f9b2a06
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-05-07 06:51:01 +0000

    update après-midi 7.5

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -2,7 +2,7 @@ const text = document.querySelector(".btn-text p");
 
 text.innerHTML = text.innerText.split('').map(
     (char, i) =>
-    `<span style="transform:rotate(${i * 7.2}deg)">${char}</span>`
+    `<span style="transform:rotate(${i * 7.1}deg)">${char}</span>`
 ).join('');
 
 // gestion du changement de page >> attendre 5 secondes
