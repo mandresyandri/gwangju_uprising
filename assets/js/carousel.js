3a4455fa0d1ed96077921cbbcfe9ee1880b77fb6
Kevin-HA-KHA <nivekah27@gmail.com>
2024-09-02 22:15:43 +0000

    Ajout transition carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,26 +2,51 @@ document.addEventListener('DOMContentLoaded', function () {
     const slides = document.querySelectorAll('.carousel-slide');
     const prevBtn = document.getElementById('prevBtn');
     const nextBtn = document.getElementById('nextBtn');
+    let slide = document.querySelector('.active');
     let currentIndex = 0;
 
     function showSlide(index) {
         slides.forEach((slide, i) => {
             slide.classList.toggle('active', i === index);
+            setTimeout(() => {
+                slide.classList.toggle('visible', i === index);
+            }, 500);
+        });
+    }
+
+    function hideSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.toggle('invisible', i === index);
         });
     }
 
     function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
+        hideSlide(currentIndex)
+        setTimeout(() => {
+            currentIndex = (currentIndex + 1) % slides.length;
+            showSlide(currentIndex);
+        }, 500);
+
     }
 
     function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
+        hideSlide(currentIndex)
+        setTimeout(() => {
+            currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+            showSlide(currentIndex);
+        }, 500);
     }
 
-    nextBtn.addEventListener('click', nextSlide);
-    prevBtn.addEventListener('click', prevSlide);
+    nextBtn.addEventListener('click', () => { 
+        clearInterval(autoSlideInterval);
+        nextSlide();
+     });
+    prevBtn.addEventListener('click', () => { 
+        clearInterval(autoSlideInterval);
+        prevSlide(); 
+    });
 
     showSlide(currentIndex);
+
+    autoSlideInterval = setInterval(nextSlide, 8000);
 });
